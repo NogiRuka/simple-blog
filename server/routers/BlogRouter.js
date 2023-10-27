e7f1ff00b727fa1b9d7e4eff150f85c364ddcbe9
“乃木流架” <1693145038@qq.com>
2023-10-28 00:28:21 +0900

    📦 编写服务端博客接口中

---
+++ server/routers/BlogRouter.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+const { db, genid } = require('../db/DBUtils')
+
+// 查询博客
+
+// 
+
+
+router.get('/test', async (req, res) => {
+
+
+
+})
+
+module.exports = router;
