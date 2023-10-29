35d28e2d0a51671cfe9324154f04832e48b62178
“乃木流架” <1693145038@qq.com>
2023-10-29 21:04:57 +0900

    🎉 初始化前端项目

---
+++ client/src/main.js
@@ -0,0 +1,15 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+
+/**
+ * axios
+ * pinia
+ * sass
+ * vue-router
+ * naive-ui
+ * wangeditor
+ */
+
+
+createApp(App).mount('#app')
